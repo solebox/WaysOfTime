54f1d8dc42de56be1c661739691dcf4843450cd0
sole <sbox@gmail.com>
2015-11-25 18:36:42 +0200

    removed some unneeded files

---
+++ provision.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+apt-get update
+pushd /srv/mapwarper
+chmod +x ./lib/vagrant/provision.sh
+./lib/vagrant/provision.sh
+popd
+pushd /srv/viewer
+sudo apt-get install -y python python-pip python-dev build-essential libpq-dev
+sudo pip install -r requirements.txt
+popd
+pushd /srv/mapwarper
+source lib/vagrant/create_user.sh
+create_user "test" "test@gmail.com" "moomoocow123"
+popd
