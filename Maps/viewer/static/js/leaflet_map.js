1c78b9e711cd9c5f362f2c621a68f6731ae50bfa
Nir Galon <nirgn975@gmail.com>
2015-11-25 20:27:48 +0200

    add side bar and logo

--- Maps/viewer/static/js/leaflet_map.js
+++ Maps/viewer/static/js/leaflet_map.js
@@ -15,30 +15,19 @@ function getLocation() {
 
 
 function showPosition(position) {
-  // set the map where the uer is (based on his coordinates
-  var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 16);
-  var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+    // set the map where the uer is (based on his coordinates
+    var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 16);
+    var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
 
-  // create a tile layers arrays for the map
-  var ovi =   L.tileLayer('http://maptile.maps.svc.ovi.com/maptiler/maptile/newest/satellite.day/{z}/{x}/{y}/256/png8'),
-      base_map = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-                  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-                  maxZoom: 18,
-                  id: 'nirgn975.cigmtjxyw000rc3knz02njkhn',
-                  accessToken: 'pk.eyJ1IjoibmlyZ245NzUiLCJhIjoiY2lnbXRqeTcxMDAwdmx6a3RueGViemV0eCJ9.3wBhw04dxzXHfd56yUfufQ'
-                }),
-      openstreets =  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'),
-      hiking =  L.tileLayer('http://osm.org.il/IsraelHiking/Tiles/{z}/{x}/{y}.png');
-
-    var baseMaps = {
-            "Base Map": base_map,
-			"OVI Satellite": ovi,
-			"OpenStreet map": openstreets,
-			"Israel Hiking Trails": hiking
-	    };
+    // create a base map layer
+    var base_map = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+                    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                    maxZoom: 18,
+                    id: 'nirgn975.cigmtjxyw000rc3knz02njkhn',
+                    accessToken: 'pk.eyJ1IjoibmlyZ245NzUiLCJhIjoiY2lnbXRqeTcxMDAwdmx6a3RueGViemV0eCJ9.3wBhw04dxzXHfd56yUfufQ'
+    });
 
     // add the widget layers to the map, and set the base map to be the default
-    L.control.layers(map.layers).addTo(map);
     base_map.addTo(map);
 
     // get all the maps from the db
