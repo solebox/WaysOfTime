13883b4dfc433fe8c3f513d8b0bf993617cd688e
sole <sbox@gmail.com>
2015-11-28 20:54:51 +0200

    added lazy loading for thumbnails :)

--- Maps/viewer/static/js/main.js
+++ Maps/viewer/static/js/main.js
@@ -1,13 +1,27 @@
-(function fetch_thumbnails(){
-    $.getJSON("getThumbs", function(data){
-        var thumbs =[];
-        var thumbs_container = $("#thumb");
-        $.each(data, function(key, thumb){
-            thumbs.push("<li class='map-thumbnail' id='thumb-" + thumb.id +
-                "'><img class='map-thumbnail' src='"+thumb.url+"' data-id='"+thumb.id+"'/></li>");
+$(document).ready(function(){
+    (function fetch_thumbnails(){
+        $.getJSON("getThumbs", function(data){
+            var thumbs =[];
+            var thumbs_container = $("#thumb");
+            $.each(data, function(key, thumb){
+                thumbs.push("<li class='map-thumbnail' id='thumb-" + thumb.id +
+                    "'><img class='lazy' data-original='"+thumb.url+"' data-id='"+thumb.id+"'/></li>");
+            });
+            $.each(thumbs, function(key, marked_up_thumb){
+                thumbs_container.append(marked_up_thumb);
+            });
+
+            if ($().lazyload) {
+                $("img.lazy").lazyload({
+                    container: $("#slider"),
+                    effect : "fadeIn"
+                });
+            } else {
+                console.log("lazyload plugin was too lazy to load");
+            }
         });
-        $.each(thumbs, function(key, marked_up_thumb){
-            thumbs_container.append(marked_up_thumb);
-        });
-    });
-})();
+    })();
+});
+
+
+
--- Maps/viewer/views/thumb_view.py
+++ Maps/viewer/views/thumb_view.py
@@ -10,13 +10,15 @@ def getAllThumbs(request):
     """
     maps = Maps.objects.all()
     url = "http://localhost:3000/uploads/{id}/thumb/{thumb_name_ext_stripped}.png"
-
+    path = "/uploads/thumb/{thumb_name_ext_stripped}.png"
     results = []
     for map in maps:
         thumb_file_name, ext = os.path.splitext(map.upload_file_name)
         results.append({'id': map.id,
                        'url': url.format(id=map.id,
-                                         thumb_name_ext_stripped=thumb_file_name)})
+                                         thumb_name_ext_stripped=thumb_file_name),
+                        'path': path.format(id=map.id,
+                                            thumb_name_ext_stripped=thumb_file_name)})
 
     return JsonResponse(results, safe=False)
 
