31427c45a7f89cd908a0eef4430a6763154597f6
Nir Galon <nirgn975@gmail.com>
2015-12-03 09:49:26 +0200

    merge search and layers

--- Maps/viewer/static/js/leaflet_map.js
+++ Maps/viewer/static/js/leaflet_map.js
@@ -1,10 +1,7 @@
-window.NLIMaps = {
-
-};
+window.NLIMaps = {};
 
 $(document).ready(function(){
     NLIMaps.map = createMap();
-
 });
 
 /**
@@ -21,8 +18,7 @@ function getLocation() {
 
 function createMap() {
     // set the map where the uer is (based on his coordinates
-    baseMap = L.map('map').setView(["31.771959", "35.217018"], 16);
-
+    var baseMap = L.map('map').setView(["31.771959", "35.217018"], 16);
 
     // create a base map layer
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -33,5 +29,3 @@ function createMap() {
     }).addTo(baseMap);
     return baseMap;
 }
-
-
--- Maps/viewer/static/js/main.js
+++ Maps/viewer/static/js/main.js
@@ -3,53 +3,67 @@ $(document).ready(function(){
 });
 
 $(function (){
-  "use strict"
-
-  $('#thumb').on('click', '.thumbnail-click', function(){
-      var imgId = $(this).find('img').data('id');
-      $('#chosenLayers').show();
-
-      $.get("/getMapById/" + imgId, function (maps) {
-         $.each(maps, function (i, map) {
-             L.tileLayer(map.url).addTo(baseMap);
-         });
-      });
-  });
-
-  $('.mdl-cell').on('click', '.mdl-button', function(){
-    var searchString = $('#fixed-search').val();
-    $('#thumb').empty();
-    if(searchString !== ""){
-        fetch_thumbnails(searchString);
-    } else {
-      fetch_thumbnails("null");
-    }
-
-  });
-
-});
-// efi
+    "use strict"
 
     var pallet =["#F44336","#E91E63","#9C27B0","#3F51B5","#03A9F4","#4CAF50","#CDDC39"];
     var ctr = 0;
 
-    $(function() {
-    $( "#sortable" ).sortable({
-      revert: true,
-      change: function(event, ui) {
-            var start_pos = ui.item.data('start_pos');
-            var index = ui.placeholder.index();
 
-          $(ui.item[0]).data("layer").setZIndex(index)
+    /**
+     *  Handle thumbnail click.
+     *  Set the image-map on the map and add it to chosen layers.
+     */
+    $('#thumb').on('click', '.thumbnail-click', function(){
+        var imgId = $(this).find('img').data('id');
+        var thumbPng = $(this).find('img').attr('src');
+        $('#chosenLayers').show();
 
+        $.get("/getMapById/" + imgId, function (maps) {
+            $.each(maps, function (i, map) {
+                L.tileLayer(map.url).addTo(window.NLIMaps.map);
+                addNewLayer(map, thumbPng);
+            });
+        });
+    });
+
+
+    /**
+     *  Handle search click.
+     *  Empty all thumbnails and call fetch_thumbnails with the search string.
+     */
+    $('.mdl-cell').on('click', '.mdl-button', function(){
+        var searchString = $('#fixed-search').val();
+        $('#thumb').empty();
+        if(searchString !== ""){
+            fetch_thumbnails(searchString);
+        } else {
+            fetch_thumbnails("null");
         }
     });
 
+
+  /**
+   *
+   */
+    $( "#sortable" ).sortable({
+        revert: true,
+        change: function(event, ui) {
+            var start_pos = ui.item.data('start_pos');
+            var index = ui.placeholder.index();
+            $(ui.item[0]).data("layer").setZIndex(index)
+        }
+    });
+
+
     $( "ul, li" ).disableSelection();
-  });
 
+
+    /**
+     *
+     * @param newMap
+     * @param pngUrl
+     */
     function addNewLayer(newMap, pngUrl) {
-
         var newLayer = L.tileLayer(newMap.url);
         var elem = $('<div id="draggable" class="mdl-card maps-card mdl-cell mdl-cell--10-col ui-state-highlight "><div class="mdl-card__media"><span><img class="nopadding" src="' + pngUrl + '" height="50" width="30"  border="0" alt="" style="padding:10px;"> <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored"> <i class="material-icons">clear</i></button></span>  </div>  <div class="mdl-card__actions"><input class="mdl-slider mdl-js-slider" type="range" min="0" max="100" value="25" tabindex="0"></input></div></div>');
         ctr++;
@@ -57,41 +71,35 @@ $(function (){
         elem.data("layer",newLayer);
         elem.find(".mdl-card__media").css("background-color", pallet[ctr%7]);
         componentHandler.upgradeDom();
+
         elem.find(".mdl-slider").on('change', function (e) {
             newLayer.setOpacity(this.value / 100.0);
         });
         newLayer.addTo(window.NLIMaps.map);
     }
 
+
+    /**
+     *
+     * @param elem
+     */
     function addFunctionality(elem) {
         $(".mdl-slider").each(function (a) {
-
             this.onchange = function (e) {
                 $.data($(".mdl-slider").get(0), "layer").setOpacity(this.value / 100.0);
-
-
             };
         })
     }
 
-
-    $('#thumb').on('click', '.thumbnail-click', function () {
-        var imgId = $(this).find('img').data('id');
-        var thumbPng = $(this).find('img').data('original');
-        $('#chosenLayers').show();
-        console.log("some thumb was clicked");
-
-        $.get("/getMapById/" + imgId, function (maps) {
-            $.each(maps, function (i, map) {
-                addNewLayer(map, thumbPng);
-                //   L.tileLayer(map.url).addTo(window.NLIMaps.map);
-            })
-        });
-    });
+});
 
 
-// efi END
-
+/**
+ *  Fetch thumbnails from the db, and place them in thumb div (in the slider)
+ *  with lazy loading.
+ *
+ * @param string - the string to search in db, "null" if there isn't any.
+ */
 function fetch_thumbnails(string){
     $.getJSON("getThumbs/" + string, function(data){
         var thumbs =[];
@@ -108,6 +116,7 @@ function fetch_thumbnails(string){
         $.each(thumbs, function(key, marked_up_thumb){
             thumbs_container.append(marked_up_thumb);
         });
+
         if ($().lazyload) {
             $("img.lazy").lazyload({
                 container: $("#slider"),
