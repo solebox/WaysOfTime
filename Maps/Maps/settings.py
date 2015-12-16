cba67af611b7a0311335f6dc7f3ffad9104aea84
sole <sbox@gmail.com>
2015-12-16 22:49:59 +0200

    added some more auth

--- Maps/Maps/settings.py
+++ Maps/Maps/settings.py
@@ -48,9 +48,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
     'PAGE_SIZE': 10
 }
 
--- Maps/viewer/views/rest_views.py
+++ Maps/viewer/views/rest_views.py
@@ -1,22 +1,23 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from django.core import serializers
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 from viewer.models import Maps, MyMaps
-from rest_framework import viewsets
 from viewer.serializers import MapsSerializer, MyMapsSerializer
 
 
-class MapsViewSet(viewsets.ModelViewSet):
+class MapsViewSet(mixins.CreateModelMixin, viewsets.ReadOnlyModelViewSet):
     """
     API view  of maps O.O
     """
     queryset = Maps.objects.all().order_by('-created_at')
     serializer_class = MapsSerializer
-
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 class MyMapsViewSet(viewsets.ModelViewSet):
     """
     API view of MyMaps O.o
+    073185ff7ee023876f323e34ad6a0aee4eca7cb3
     """
     queryset = MyMaps.objects.all().order_by()
     serializer_class = MyMapsSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
