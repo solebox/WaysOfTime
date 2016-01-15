ac264d5bee24f5de41ed272cd85e90e88aa6cd9b
Nir Galon <nirgn975@gmail.com>
2016-01-15 17:11:20 +0200

    Edit css

--- Maps/viewer/static/js/vendor/src/draw/DrawToolbar.js
+++ Maps/viewer/static/js/vendor/src/draw/DrawToolbar.js
@@ -5,11 +5,7 @@ L.DrawToolbar = L.Toolbar.extend({
 	},
 
 	options: {
-		//polyline: {},
-		//polygon: {},
 		rectangle: {},
-		//circle: {},
-		//marker: {}
 	},
 
 	initialize: function (options) {
@@ -29,29 +25,9 @@ L.DrawToolbar = L.Toolbar.extend({
 	getModeHandlers: function (map) {
 		return [
 			{
-				enabled: this.options.polyline,
-				handler: new L.Draw.Polyline(map, this.options.polyline),
-				title: L.drawLocal.draw.toolbar.buttons.polyline
-			},
-			{
-				enabled: this.options.polygon,
-				handler: new L.Draw.Polygon(map, this.options.polygon),
-				title: L.drawLocal.draw.toolbar.buttons.polygon
-			},
-			{
 				enabled: this.options.rectangle,
 				handler: new L.Draw.Rectangle(map, this.options.rectangle),
 				title: L.drawLocal.draw.toolbar.buttons.rectangle
-			},
-			{
-				enabled: this.options.circle,
-				handler: new L.Draw.Circle(map, this.options.circle),
-				title: L.drawLocal.draw.toolbar.buttons.circle
-			},
-			{
-				enabled: this.options.marker,
-				handler: new L.Draw.Marker(map, this.options.marker),
-				title: L.drawLocal.draw.toolbar.buttons.marker
 			}
 		];
 	},
