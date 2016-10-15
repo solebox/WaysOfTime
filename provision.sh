584a65d17b52818fa459ae34d16368e094af3f6c
solebox <solekiller@gmail.com>
2016-10-15 09:26:15 +0300

    added some provisioning spice

--- provision.sh
+++ provision.sh
@@ -5,9 +5,11 @@ chmod +x ./lib/vagrant/provision.sh
 ./lib/vagrant/provision.sh
 popd
 pushd /srv/Maps
-sudo apt-get install -y python python-pip python-dev build-essential libpq-dev python3-dev
+sudo apt-get install -y python python-pip python-dev build-essential libpq-dev python3-dev --force-yes
 popd
+pushd /srv
 sudo pip install -r requirements.txt
+popd
 pushd /srv/mapwarper
 source lib/vagrant/create_user.sh
 create_user "test" "test@gmail.com" "moomoocow123"
