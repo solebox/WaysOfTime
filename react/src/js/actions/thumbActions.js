fc407cbf73b38f5f593e1e66b0b0b2c4a6373c13
solebox <solekiller@gmail.com>
2016-10-19 08:59:57 +0300

    static_example is ready , just npm run dev in react dir

--- react/src/js/actions/thumbActions.js
+++ react/src/js/actions/thumbActions.js
@@ -3,13 +3,9 @@ import axios from "axios";
 
 export function fetchThumbs(){
 	return (dispatch) => {
-		axios.get("getThumbs/list")
-			.then((response)=>{
-				dispatch({type: "FETCH_THUMBS_FULFILLED", payload: response.data});
-			})
-			.catch((error)=>{
-				dispatch({type: "FETCH_THUMBS_REJECTED", payload: error});
-			})
+		const data = [{"upload_file_name": "alice.jpg", "id": 1, "url": "http://localhost:3000/uploads/1/thumb/alice.png", "title": "first"}, {"upload_file_name": "babyj.png", "id": 2, "url": "http://localhost:3000/uploads/2/thumb/babyj.png", "title": "second"}, {"upload_file_name": "kest.png", "id": 3, "url": "http://localhost:3000/uploads/3/thumb/kest.png", "title": "third"}, {"upload_file_name": "telp.png", "id": 4, "url": "http://localhost:3000/uploads/4/thumb/telp.png", "title": "fourth"}]
+		dispatch({type: "FETCH_THUMBS_FULFILLED", payload: data});
+			
 	}
 }
 
