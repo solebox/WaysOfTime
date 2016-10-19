9cfaa23dc2048b54a6dff9dca2612fdba9b07eff
solebox <solekiller@gmail.com>
2016-10-19 12:33:36 +0300

    softlinking instead of copy

--- react/src/js/components/Slider/Thumbnail.js
+++ react/src/js/components/Slider/Thumbnail.js
@@ -11,27 +11,26 @@ export default class Thumbnail extends React.Component {
 
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(e)  {
-    e.preventDefault();
+  handleClickz()  {
     console.log("clicked!!");
+    alert(1);
   }
 
   render() {
   	const { map_id, map_url, map_title } = this.props;
     return (
-      <li class='thumbnail-click' id={"map-" + map_id } onClick={this.handleClick} >
+      <li onClick={this.handleClickz.bind(this)} class='thumbnail-click' id={"map-" + map_id } >
 	    	<div class='demo-card-image mdl-card mdl-shadow--2dp'>
 		        <CardMenu map_id={map_id} />
-		        <img src={ map_url } data-id={ map_id } onClick={this.handleClick} />
+		        <img src={ map_url } data-id={ map_id } />
 		        <div class='mdl-card__actions' >
 		            <span class='demo-card-image__filename'>{ map_title }</span>
 		            <input class="invisible thumb-slider" type="range" min="0" max="100" value="100" tabindex="0" />
 		        </div>
 	        </div>
-	</li>
+	     </li>
     );
   }
 }
